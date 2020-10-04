ce94eda9f865589a26e8256a3bb137a44b20e879
Skylar Wong <li.q.wong@gmail.com>
2020-10-04 01:53:48 -0500

    add first-post page and Link component

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const FirstPost = () => {
+    return (  
+        <>
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+            
+        </h2>
+        </>
+    );
+}
+ 
+export default FirstPost;
